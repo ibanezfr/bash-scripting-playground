867d3c4028866dc473446fcece15e4a673e0d6ab
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-09 23:04:09 -0300

    updates git_to_vm

--- sync_asm_files_git_to_VM.sh
+++ sync_asm_files_git_to_VM.sh
@@ -8,34 +8,23 @@ dir_destino="/tmp/freedos_mount/SRC/AsmCode"
 archivos_nuevos=0
 archivos_modificados=0
 
-# Verificar si hay archivos .ASM en el directorio de destino
+# Verificar si hay archivos .ASM en la carpeta de destino
 if ls "$dir_destino"/*.ASM 1> /dev/null 2>&1; then
-    # Iterar sobre los archivos .ASM en el directorio de destino
+    # Iterar sobre los archivos .ASM en la carpeta de destino
     for archivo_destino in "$dir_destino"/*.ASM; do
         archivo_nombre=$(basename "$archivo_destino")
         archivo_origen="$dir_origen/$archivo_nombre"
 
-        # Calcular los hashes de los archivos
-        hash_destino=$(md5sum "$archivo_destino" | awk '{print $1}')
-
-        if [ -e "$archivo_origen" ]; then
-            hash_origen=$(md5sum "$archivo_origen" | awk '{print $1}')
-        else
-            hash_origen=""
-        fi
-
-        # Comprobar si los hashes son diferentes
-        if [ "$hash_destino" != "$hash_origen" ]; then
-            if [ -e "$archivo_origen" ]; then
-                archivos_modificados=$((archivos_modificados + 1))
-            else
-                archivos_nuevos=$((archivos_nuevos + 1))
-            fi
-            cp "$archivo_destino" "$archivo_origen"
+        # Comprobar si el archivo de origen existe y si es diferente del archivo de destino
+        if [ -e "$archivo_origen" ] && [ "$archivo_destino" -nt "$archivo_origen" ]; then
+            archivos_modificados=$((archivos_modificados + 1))
+            cp "$archivo_origen" "$archivo_destino"
         fi
     done
 else
-    echo "No se encontraron archivos .ASM en $dir_destino"
+    # Si no existen archivos .ASM en la carpeta de destino, copiar todos los archivos .ASM desde la carpeta de origen
+    cp "$dir_origen"/*.ASM "$dir_destino"
+    archivos_nuevos=$(ls -1 "$dir_origen"/*.ASM | wc -l)
 fi
 
 # Mostrar el informe
