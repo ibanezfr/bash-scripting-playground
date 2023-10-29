d74b8f1101da7b50d1d60319586f0eb0f085d30d
francoibanezweb <francoibanez.dev@gmail.com>
2023-10-29 17:36:20 -0300

    feat: update inciso-6.sh description

--- practicas-SO/tp2/inciso-6.sh
+++ practicas-SO/tp2/inciso-6.sh
@@ -2,10 +2,11 @@
 # Nombre del Script: inciso-6.sh
 # Autor: Franco E. Ibañez
 # Fecha de creación: 29 de octubre de 2023
-# Descripción: Este script descomprime un archivo .tar.gz, extrae ciertas
-#              líneas de interés de un archivo recién descomprimido, y las
-#              inserta dentro de un archivo .txt .
-# Uso:
+# Descripción: Este script se centra en descomprimir un archivo .tar.gz y
+#              buscar líneas de interés en un archivo específico dentro de la
+#              carpeta descomprimida. Luego, guarda estas líneas en un archivo
+#              de texto y lo comprime en formato .tar.gz.
+# Uso: ./inciso-6.sh
 
 # Comienzo
 
