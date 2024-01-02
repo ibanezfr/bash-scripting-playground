b7ae1a82bd9ae4fc09d78e94df579dbd3f99e4c3
francoibanezweb <francoibanez.dev@gmail.com>
2024-01-02 16:56:36 -0300

    feat: modularize inciso-02 solution

--- practicas-SO/tp4/inciso-02.c
+++ practicas-SO/tp4/inciso-02.c
@@ -1,38 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void imprimir(int V[10]);
+void ordenar(int V[10]);
+
 int main() {
   int vector[10];
-  int i, j;
+  int i;
 
   for (i = 0; i < 10; i++) {
      printf("Ingrese número %d > ", i + 1);
      scanf("%d", &vector[i]);
   }
 
+  imprimir(vector);
+
+  ordenar(vector);
+
+  imprimir(vector);
+
+  exit(0);
+}
+
+void ordenar(int V[10]) {
+  int i,j;
+
   for ( i = 0; i < 9; i++) {
-    for ( j = i + 1; j <= 9; j++) {
-      int aux;
-      if(vector[i] > vector[j]) {
-        aux = vector[i];
-        vector[i] = vector[j];
-        vector[j] = aux;
+      for ( j = i + 1; j <= 9; j++) {
+        int aux;
+        if(V[i] > V[j]) {
+          aux = V[i];
+          V[i] = V[j];
+          V[j] = aux;
+        }
       }
     }
-  }
+}
+
+
+void imprimir(int V[10]) {
+  int i;
 
   for (i = 0; i < 10; i++) {
      if ( i == 0) {
        printf("\n[ ");
      }
      if ( i == 9) {
-       printf("%d ]\n\n", vector[i]);
+       printf("%d ]\n\n", V[i]);
      }
      else {
-     printf("%d, ", vector[i]);
+     printf("%d, ", V[i]);
      }
   }
-
-  exit(0);
 }
 
