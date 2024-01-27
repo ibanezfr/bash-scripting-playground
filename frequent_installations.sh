a74836fc6d8f49eeb1db58c3488c75962db33951
francoibanezweb <francoibanez.dev@gmail.com>
2024-01-27 19:13:34 -0300

    feat: add frequent_installation

--- frequent_installations.sh
+++ frequent_installations.sh
@@ -26,18 +26,25 @@ install_package() {
 }
 
 # Install MegaCMD
-mega_cmd_url="https://mega.nz/linux/repo/Debian_12/amd64/megacmd-Debian_12_amd64.deb"
+mega_cmd_url="https://mega.nz/linux/repo/Debian_12/amd64/megacmd-Debian_12\
+		_amd64.deb"
 install_package "$mega_cmd_url"
 
 # Install Brave Browser
-brave_key_url="https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg"
-sudo curl -fsSLo "/usr/share/keyrings/brave-browser-archive-keyring.gpg" "$brave_key_url"
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+brave_key_url="https://brave-browser-apt-release.s3.brave.com/brave-browser-\
+		archive-keyring.gpg"
+sudo curl -fsSLo "/usr/share/keyrings/brave-browser-archive-keyring.gpg" \
+		  "$brave_key_url"
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] \
+	https://brave-browser-apt-release.s3.brave.com/ stable main" 
+	| sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
 
 # Install other packages
-packages=("neofetch" "build-essential" "keepassxc" "vim-gtk3" "cscope" "fonts-dejavu" "gnome-icon-theme" "vim-doc" "gh" "git")
+packages=("neofetch" "build-essential" "keepassxc" "vim-gtk3" "cscope" 
+	    "fonts-dejavu" "gnome-icon-theme" "vim-doc" "gh" "git")
 for package in "${packages[@]}"; do
     sudo apt install "$package"
 done
